cd94278d6292e0fd95e877e829718b22a2f7a404
The Jack of All Trades <thejackofalltrades09@gmail.com>
2026-08-19 17:35:33 -0400

    Update extensions.py

--- Problem Set 1/extensions.py
+++ Problem Set 1/extensions.py
@@ -1,16 +1,26 @@
-filename = input("File name: ").strip().lower()
+def main():
+    file_name = input("File name: ").strip().lower()
 
-if filename.endswith(".gif"):
-    print("image/gif")
-elif filename.endswith(".jpg") or filename.endswith(".jpeg"):
-    print("image/jpeg")
-elif filename.endswith(".png"):
-    print("image/png")
-elif filename.endswith(".pdf"):
-    print("application/pdf")
-elif filename.endswith(".txt"):
-    print("text/plain")
-elif filename.endswith(".zip"):
-    print("application/zip")
-else:
-    print("application/octet-stream")
+    if file_name.endswith(".jpeg") or file_name.endswith(".jpg"):
+        print("image/jpeg")
+
+    elif file_name.endswith(".gif"):
+        print("image/gif")
+
+    elif file_name.endswith(".png"):
+        print("image/png")
+
+    elif file_name.endswith(".pdf"):
+        print("application/pdf")
+
+    elif file_name.endswith(".txt"):
+        print("text/plain")
+
+    elif file_name.endswith(".zip"):
+        print("application/zip")
+
+    else:
+        print("application/octet-stream")
+
+
+main()
