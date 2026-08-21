e43e7fd1477a6d046f4cf001d503d2b79002d988
The Jack of All Trades <thejackofalltrades09@gmail.com>
2026-08-21 18:16:15 -0400

    Update camel.py

--- Problem Set 2/camel.py
+++ Problem Set 2/camel.py
@@ -1,1 +1,11 @@
+camel_case = input("camelCase: ")
 
+snake_case = ""
+for letter in camel_case:
+    if letter.isupper() == True:
+        snake_case = f"{snake_case}_{letter.lower()}"
+    else:
+        snake_case = f"{snake_case}{letter}"
+        pass
+
+print(f"snake_case: {snake_case}")
