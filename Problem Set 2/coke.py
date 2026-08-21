a8de64da2f07ee505ead8e57f40c4a36d877b958
The Jack of All Trades <thejackofalltrades09@gmail.com>
2026-08-21 18:27:09 -0400

    Update coke.py

--- Problem Set 2/coke.py
+++ Problem Set 2/coke.py
@@ -1,1 +1,11 @@
-
+total = 50
+while True:
+    print(f"Amount Due: {total}")
+    money = int(input("Insert Coin: "))
+    if money in (25, 10, 5):
+        total = total - money
+        if total <= 0:
+            print(f"Change Owed: {abs(total)}")
+            break
+    else:
+        pass
