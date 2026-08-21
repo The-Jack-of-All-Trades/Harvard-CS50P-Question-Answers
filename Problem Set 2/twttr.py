032e944e16dbc62085341de887fb90098a0aa7f6
The Jack of All Trades <thejackofalltrades09@gmail.com>
2026-08-21 18:36:16 -0400

    Update twttr.py

--- Problem Set 2/twttr.py
+++ Problem Set 2/twttr.py
@@ -1,1 +1,10 @@
+phrase = input("Input: ")
 
+output = ""
+for letter in phrase:
+    if letter.lower() in ("a", "e", "i", "o", "u"):
+        pass
+    else:
+        output = f"{output}{letter}"
+
+print(f"Output: {output}")
