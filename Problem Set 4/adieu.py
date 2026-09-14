8dc09ed3963abfce00a8f6e5adad4be134504581
The Jack of All Trades <thejackofalltrades09@gmail.com>
2026-09-14 18:13:02 -0400

    Create adieu.py

---
+++ Problem Set 4/adieu.py
@@ -0,0 +1,29 @@
+import sys
+names = []
+
+def nameprint(names):
+    total = 0
+
+    print("\nAdieu, adiue, to", end="")
+    for thenames in names:
+        total += 1
+        if total != len(names):
+            print(f" {thenames},", end="")
+        elif total == len(names) and len(names) == 1:
+            print(f" {thenames}")
+        elif total == len(names):
+            print(f" and {thenames}", end="")
+
+
+def ending(names):
+    nameprint(names)
+    sys.exit()
+
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)
+    except EOFError:
+        break
+
+ending(names)
