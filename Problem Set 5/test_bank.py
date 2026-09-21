6ae6452e1e61ba7cd9a662d189f5436c2f884527
The Jack of All Trades <thejackofalltrades09@gmail.com>
2026-09-21 17:37:14 -0400

    Create test_bank.py

---
+++ Problem Set 5/test_bank.py
@@ -0,0 +1,13 @@
+from cs50p import value
+
+def test_symbols():
+    assert value("hiya folks!!!") == "$20"
+
+def test_nums():
+    assert value("1234hello") == "$100"
+
+def test_nothing():
+    assert value("") == "$100"
+
+def test_hello():
+    assert value("hello there") == "$0"
